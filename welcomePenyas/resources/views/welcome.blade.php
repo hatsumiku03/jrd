d1338d71481f4f285045b06ca42b826f5585cb60
Joselito <josalbarr@alu.edu.gva.es>
2025-01-16 09:30:06 +0100

    end the home please

--- welcomePenyas/resources/views/welcome.blade.php
+++ welcomePenyas/resources/views/welcome.blade.php
@@ -45,7 +45,24 @@
             @livewire('carousel')
         
         <!-- About -->
-        weq
+            <div class="text-white bg-black">
+            <p>
+                Unas de las fiestas más esperadas por la juventud vallduxense son Les Penyes en Festes, nacidas en el año 1982, se celebran la primera semana del mes de agosto y están organizadas por las peñas de jóvenes de la Vall d’Uixó.
+            </p>
+
+            <p>
+                Actualmente, la fiesta ha cambiado bastante desde sus comienzos. El tradicional chupinazo, cohete que anuncia el inicio de las fiestas, se dispara desde el balcón del Ayuntamiento al más puro estilo de San Fermín, donde el color en las blusas de los peñistas, la pólvora y el agua dan la bienvenida a la gran semana.
+            </p>
+
+            <p>
+                Todas las tardes, los peñistas se reúnen en la plaza del mercado, con sus respectivas blusas acreditativas, creando un gran arcoíris de color que llenará la plaza en la que estarán presentes los representantes de las 50 peñas existentes. En esta plaza, rodeada de “cadafals”, tiene lugar el famoso Concurso de Ganaderías.
+            </p>
+
+            <p>
+                Además de actos taurinos, en estas fiestas populares también se pueden disfrutar de conciertos, verbenas, discomóviles, pasacalles, desfiles de disfraces, cercaviles recorriendo la ciudad con charangas, concursos de gastronomía local, cenas de hermandad y numerosos espectáculos pirotécnicos. 
+            </p>
+
+            </div>
         <!-- Form -->
 
         <!-- Maps -->
