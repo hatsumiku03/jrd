7df5a00b9504073f86c93300fdf9d747718fa573
Joselito <josalbarr@alu.edu.gva.es>
2025-03-04 09:44:45 +0100

    finised

--- welcomePenyas/database/migrations/2024_10_08_175507_create_draws_table.php
+++ welcomePenyas/database/migrations/2024_10_08_175507_create_draws_table.php
@@ -9,16 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    /* Esto esta mal, tiene que quedarse un registro de otros años */
     public function up(): void
     {
         Schema::create('draws', function (Blueprint $table) {
             $table->id();
             $table->foreignId('crew_id')->constrained(
                 table: 'crews', indexName: 'crewsDraws_id'
-            );
+            )->cascadeOnDelete();
             $table->foreignId('location_id')->constrained(
                 table: 'locations', indexName: 'locationDraws_id'
-            );
+            )->cascadeOnDelete();
             $table->timestamps();
         });
     }
